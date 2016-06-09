c5faa97d49a31cf5e3d11a9e717af4139ed0732a
bhav97 <anubhav7991@gmail.com>
2016-06-09 17:03:34 +0530

    broken build: first attempt to make multiple requests

--- server.js
+++ server.js
@@ -29,72 +29,53 @@ request(url, function(error, response, html){
           fifth : ""
         }
 
+        var latest_news = [];
     $('.latest_postnav').filter(function(){
         var data = $(this);
         //first news item
-        console.log(data.children().first().text().replace(/\n              /g, "").replace(/  /g, ""));
-        first = {
-          title : data.children().eq(0).text().replace(/\n              /g, "").replace(/  /g, ""),
-          category : data.children().eq(0).children().first().children().first().attr('href').split("/")[0],
-          linkk_news : data.children().eq(0).children().first().children().first().attr('href'),
-          link_image : data.children().eq(0).children().first().children().first().children().first().attr('src'),
-        };
+        for(var i=0; i < 5; i++) {
+          latest_news.push({
+            id : data.children().eq(i).children().first().children().first().attr('href').split("--")[1],
+            title : data.children().eq(i).text().replace(/\n              /g, "").replace(/  /g, ""),
+            category : data.children().eq(i).children().first().children().first().attr('href').split("/")[0],
+            language : data.children().eq(i).children().first().children().first().attr('href').split("--")[2],
+            link_news : "http://www.medhajnews.in/" + data.children().eq(i).children().first().children().first().attr('href'),
+            link_image : data.children().eq(i).children().first().children().first().children().first().attr('src'),
+            author : "",
+            time : "",
+            area : "",
+            likes : "",
+            dislikes : "",
+            article_content : "",
+            inline_image_links : []
+          });
+        }
+        var len = latest_news.length;
+        for(var i=0; i < len; i++) {
+          request(latest_news[i].link_news, function(err, response, html){
+            if(!err){
+              var $ = cheerio.load(html);
+              console.log('loaded article ' + i);
 
-        second = {
-          title : data.children().eq(1).text().replace(/\n              /g, "").replace(/  /g, ""),
-          category : data.children().eq(1).children().first().children().first().attr('href').split("/")[0],
-          linkk_news : data.children().eq(1).children().first().children().first().attr('href'),
-          link_image : data.children().eq(1).children().first().children().first().children().first().attr('src'),
-        };
-
-        third = {
-          title : data.children().eq(2).text().replace(/\n              /g, "").replace(/  /g, ""),
-          category : data.children().eq(2).children().first().children().first().attr('href').split("/")[0],
-          linkk_news : data.children().eq(2).children().first().children().first().attr('href'),
-          link_image : data.children().eq(2).children().first().children().first().children().first().attr('src'),
-        };
-
-        fourth = {
-          title : data.children().eq(3).text().replace(/\n              /g, "").replace(/  /g, ""),
-          category : data.children().eq(3).children().first().children().first().attr('href').split("/")[0],
-          linkk_news : data.children().eq(3).children().first().children().first().attr('href'),
-          link_image : data.children().eq(3).children().first().children().first().children().first().attr('src'),
-        };
-
-        fifth = {
-          title : data.children().eq(4).text().replace(/\n              /g, "").replace(/  /g, ""),
-          category : data.children().eq(4).children().first().children().first().attr('href').split("/")[0],
-          linkk_news : data.children().eq(4).children().first().children().first().attr('href'),
-          link_image : data.children().eq(4).children().first().children().first().children().first().attr('src'),
-        };
-        console.log('Got Data!');
-
-        json.first = first;
-        json.second = second;
-        json.third = third;
-        json.fourth = fourth;
-        json.fifth = fifth;
-
+              $('.single_page').filter(function(){
+                var data = $(this);
+                latest_news[i].time = data.children().eq(2).children().eq(1).text();
+                latest_news[i].author = data.children().eq(2).children().eq(0).text().replace(/\n/g, "").replace("              ","").replace("               ","");
+                latest_news[i].area = data.children().eq(3).children().eq(1).text();
+                for(var j=0; j < data.children().eq(3).children("p").slice(1,-1).length; j++) {
+                  latest_news.article_content = latest_news.article_content +  data.children().eq(3).children("p").slice(1,-1)[i];
+                }
+              })
+            }
+          });
+        }
         console.log('JSON ready');
 
     })
 }
-// Parameter 1 :  output.json - this is what the created filename will be called
-// Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
-// Parameter 3 :  callback function - a callback function to let us know the status of our function
+res.end(JSON.stringify(latest_news));
 
-// fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-//
-//
-//     console.log('JSON response sent');
-//
-// })
-
-// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
-// res.send('Check your console!')
-res.end(JSON.stringify(json));
-
-    }) ;
+    });
 })
 app.listen(port)
 
