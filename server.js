18738234eec753f1ad0528140e3480fc5979c9a1
bhav97 <anubhav7991@gmail.com>
2016-07-01 00:07:50 +0530

    update models

--- server.js
+++ server.js
@@ -41,8 +41,12 @@ function ads(req, res) {
   latest_ads = [];
   for(var i=0; i < 5; i++) {
       latest_ads.push({
-        ad_url : "google.com",
-        ad_image_url : "https://think.storage.googleapis.com/images/youtube-homepage-ads-lg.jpg"
+        id : 123 + i,
+        url : "apple.com",
+        link_image : "https://si.wsj.net/public/resources/images/BN-NF598_ptech0_H_20160323105214.jpg",
+        content : "Because if its not an iPhone, its not an iPhone",
+        title : "iPhone 7S Plus",
+        provider : "Apple"
         });
   }
   res.end(JSON.stringify(latest_ads));
@@ -58,7 +62,11 @@ function data(req, res) {
           var $ = cheerio.load(html);
           console.log('Cheerio loaded HTML');
 
-
+          var comment1 = {
+            "id" : 12321,
+            "body" : "apkuploadplznow",
+            "user" : 143
+          }
           var latest_news = [];
       $('.latest_postnav').filter(function(){
           var data = $(this);
@@ -69,15 +77,17 @@ function data(req, res) {
                 title : data.children().eq(i).text().replace(/\n              /g, "").replace(/  /g, ""),
                 category : data.children().eq(i).children().first().children().first().attr('href').split("/")[0],
                 language : data.children().eq(i).children().first().children().first().attr('href').split("--")[2],
-                link_news : "http://www.medhajnews.in/" + data.children().eq(i).children().first().children().first().attr('href'),
-                link_image : data.children().eq(i).children().first().children().first().children().first().attr('src'),
+                url : "http://www.medhajnews.in/" + data.children().eq(i).children().first().children().first().attr('href'),
+                link_image : [data.children().eq(i).children().first().children().first().children().first().attr('src')],
                 author : "Mr Incredible",
                 time : "9 hours ago",
                 date : "19 Jun",
                 area : "New Delhi",
-                likes : "9",
-                dislikes : "6",
-                article_content : "Movie Shorgul, based on the Muzzaffarnagar riots of 2013, has been unofficially banned in the western part of Uttar Pradesh.<<>>According to reports, more than two dozens of theatres in Muffarnagar and Meerut have refused to exhibit the movie. The theatres cited security reasons for not running the Shorgul, scheduled to release on June 24, in their cinema halls.<<>>Manoj Vjpayee, district entertainer officer, pointed out that none the single-small screen cinema halls and multiplex in Muffarnagar had seek approached him to seek permission for exhibiting the film.<<>>Meanwhile, BJP MLA Sangeet Som expressed anger over the story of Shorgul. He reiterated that the director and producers were damaging the reputation of Muffarnagar. "
+                likes : 9,
+                comments : [comment1, comment1],
+                dislikes : 6,
+                type : "large",
+                content : "Movie Shorgul, based on the Muzzaffarnagar riots of 2013, has been unofficially banned in the western part of Uttar Pradesh.<<>>According to reports, more than two dozens of theatres in Muffarnagar and Meerut have refused to exhibit the movie. The theatres cited security reasons for not running the Shorgul, scheduled to release on June 24, in their cinema halls.<<>>Manoj Vjpayee, district entertainer officer, pointed out that none the single-small screen cinema halls and multiplex in Muffarnagar had seek approached him to seek permission for exhibiting the film.<<>>Meanwhile, BJP MLA Sangeet Som expressed anger over the story of Shorgul. He reiterated that the director and producers were damaging the reputation of Muffarnagar. "
               });
           }
           console.log('JSON ready');
