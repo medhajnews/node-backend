3c9840685540945e8cdb15c9b11f62bbb6302efb
bhav97 <anubhav7991@gmail.com>
2016-07-01 12:52:30 +0530

    added sample photo article

--- server.js
+++ server.js
@@ -34,7 +34,52 @@ app.get('/ads', function(req, res){
   ads(req, res);
 })
 
+app.get('/photos', function(req, res) {
+  photos(req, res);
+})
+
 app.listen(port)
+
+function photos(req, res) {
+  res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
+  latest_photos = [];
+  var comment1 = {
+    "id" : 12321,
+    "body" : "apkuploadplznow",
+    "user" : 143
+  }
+  for(var i=0; i < 5; i++) {
+      latest_photos.push({
+        id : 123 + i,
+        url : "google.com",
+        content : ["Bollywood male actors may not be as loyal partner but when it comes to their love for kids, these bollywood stars do present themselves as the best fathers. They know that they are star for the world but family comes first and hence they do their bit for kids. Have a look at bollywoods top fathers.",
+      "No list in complete without the ultimate Shehanshah, The big B! Having played the cantankerous Bhaskor Banerjee in Piku, he is quite the opposite in real life. Father to daughter Shweta Nanda and son Abhishek Bachchan and now grandfather to Navya Naveli, Agastya Nanda and Aaradhya Bachchan.",
+    "Surprised? He may not be an Actor-Parent but his immense love and dedication for his children is wonderful. He has daughter Pooja Bhatt and son Rahul from his previous marriage and daughters Shaheen Bhatt and Alia Bhatt from his second marriage to Soni Razdan. We absolutely admire the way he takes a stand everytime his children are bullied. The way he openly spoke against the ‘Udta Punjab ban’ or when the ‘Dumb Alia’ jokes did the rounds. He silenced the trolls and gave out a clear message to not mess with his children! At his wittiest best!",
+  "The heartthrob duo is the most dashing father-son team in the B-town. The Kapoor family has maintained their tradition of grooming and present once again a superstar to the industry. As Raj Kapoor did to Rishi, he too has trained Ranbir perfectly on performance and star quotient as well.",
+"Though he is in the news for his controversial divorce to wife Sussanne and alleged relationship with Kangana Ranaut, this hasn’t stopped him from being a hands on dad to his sons Hrehaan and Hridhaan. Though his sons live with their mother, Hrithik has visitation rights and often whisks them away for holidays abroad and indulges them with food and movies when in the city.",
+"He is the best in everything he does, including parenthood! King Khan is a doting parent to his 3 children Aryan, Suhana and AbRam. From helping do their homework, to having his children accompany him for IPL Matches, picking up fights with anyone who dare touch them, to being their cheerleader at their graduation to loving them so passionately.",
+"This Khan superstar is quite protective about his 3 children – Junaid, Ira and Azad. He has Junaid and Ira from his previous marriage to Reema and Azad with current wife Kiran Rao. But this hasn’t deterred him from showering equal amounts of love and affection on all his children. Recently his daughter Ira expressed to go on a Euro trip with her friends and Aamir allowed on a condition that she has to update him every 4 hours."],
+
+        title : "Best B-Town Fathers",
+        link_image : ["https://medhaj-staging.s3.amazonaws.com/uploads/photo/data_file/45069/star_dads.png",
+      "https://medhaj-staging.s3.amazonaws.com/uploads/photo/share_img/Amitabh_Bachcha.png",
+    "https://medhaj-staging.s3.amazonaws.com/uploads/photo/share_img/Alia_father.png",
+  "https://medhaj-staging.s3.amazonaws.com/uploads/photo/share_img/Rishi_ranbir.png",
+"https://medhaj-staging.s3.amazonaws.com/uploads/photo/share_img/Hrithik_sons.png",
+"https://medhaj-staging.s3.amazonaws.com/uploads/photo/share_img/Sharukh_kids.png",
+"https://medhaj-staging.s3.amazonaws.com/uploads/photo/share_img/amir_family.png"],
+        author : "Ravi Dubey",
+        category : "Entertainment",
+        language : "en",
+        date : "",
+        time : "19 Jun",
+        likes : 56,
+        dislikes : 9,
+        comments : [comment1, comment1]
+        });
+  }
+  res.end(JSON.stringify(latest_photos));
+}
 
 function ads(req, res) {
   res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
